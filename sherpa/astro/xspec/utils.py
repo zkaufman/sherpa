ffc4cc4ec3004e4560b380aa4d34537664aa702e
Omar Laurino <olaurino@cfa.harvard.edu>
2017-12-18 11:07:47 -0500

    make xspec version a module level constant

--- sherpa/astro/xspec/utils.py
+++ sherpa/astro/xspec/utils.py
@@ -22,6 +22,8 @@ from . import _xspec
 
 __all__ = ['ModelMeta', 'include_if', 'version_at_least']
 
+XSPEC_VERSION = LooseVersion(_xspec.get_xsversion())
+
 
 class ModelMeta(type):
     """
@@ -101,8 +103,7 @@ class version_at_least(include_if):
 
     def __init__(self, version_string):
         self.version_string = version_string
-        xspec_version = LooseVersion(_xspec.get_xsversion())
-        include_if.__init__(self, xspec_version >= LooseVersion(version_string))
+        include_if.__init__(self, XSPEC_VERSION >= LooseVersion(version_string))
 
     def get_message(self, model_class):
         return self.DISABLED_MODEL_MESSAGE.format(model_class.__name__, self.version_string)
